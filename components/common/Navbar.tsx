2fe9acc3eef607f02488a32a9a245a3f211f7ebb
Watcher <watchereth10@gmail.com>
2025-08-03 15:15:09 -0700

    animated fab

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -4,22 +4,20 @@ import React from 'react'
 import { SymbolView, SymbolViewProps, SFSymbol } from 'expo-symbols';
 import { useRouter } from 'expo-router'
 import { AnimatedPressable } from '../ui/AnimatedPressable'
+import { LinearGradient } from 'expo-linear-gradient'
 const Navbar = () => {
   const router = useRouter()
 
   return (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 16, width: '100%', backgroundColor: '#F8F8F8', paddingTop: 60 }}>
+    <LinearGradient colors={["#fefefe", "rgba(255, 255, 255, 0)"]} style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 16, width: '100%', paddingTop: 60, position: "absolute", top: 0, zIndex: 1000 }}>
         <AnimatedPressable contentStyle={{ flexDirection: 'row', alignItems: 'center' }} onPress={() => {}}>
-            <Image source={{uri: 'https://pbs.twimg.com/profile_images/1749354610261979136/44nFYEPi_400x400.jpg'}} style={{ width: 28, height: 28, borderRadius: 14, marginRight: 10 }} />
-            <Text style={{ fontFamily: 'SFPro-Bold', fontSize: 20, color: '#808080' }}>Watchereth_</Text>
+            <Text style={{ fontFamily: 'SFPro-Bold', fontSize: 26, color: '#808080' }}>Reveries</Text>
         </AnimatedPressable>
-        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
-          <AnimatedPressable onPress={() => {}}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 13 }}>
         <SymbolView name="qrcode" size={24} tintColor={"#808080"} weight="bold"/>
-        </AnimatedPressable>
-        <SymbolView name="magnifyingglass" size={24} tintColor={"#808080"} weight="bold"/>
+        <SymbolView name="ellipsis" size={24} tintColor={"#808080"} weight="bold"/>
         </View>    
-    </View>
+    </LinearGradient>
   )
 }
 
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../common/Navbar'
-import { ScrollView, View } from 'react-native'
+import { View } from 'react-native'
 import InfiniteCanvas from '../ui/InfiniteCanvas'
+import FloatingActionButton from '../ui/AnimatedFab'
 
 const HomePage = () => {
+
+
+
   return (
     <View style={{ flex: 1 }}>
+      <Navbar/>
           <InfiniteCanvas/>
+          <FloatingActionButton />
+
     </View>
   )
 }
